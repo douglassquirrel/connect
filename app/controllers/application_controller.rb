ea464e8763fea8778325135addfc4ca287d5232a
douglassquirrel <squirrel@blindpages.com>
2010-02-14 08:22:17 +0000

    Use RESTful drop and take

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  #protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 
-  before_filter :login_required, :except=>[:login, :logout]
+  #before_filter :login_required, :except=>[:login, :logout]
 
 
   def login_required
--- app/controllers/my_tasks_controller.rb
+++ app/controllers/my_tasks_controller.rb
@@ -9,38 +9,6 @@ class MyTasksController < ApplicationController
       format.html # index.html.erb
     end
   end
-
-  # GET /my_tasks/take/:id
-  def take
-    @task = Task.find(params[:id])
-    @task.assignee = session[:user]
-    
-    if @task.save then
-      flash[:notice] = 'Task assigned to you!'
-    else
-      flash[:warning] = 'Unable to assign task to you!'
-    end
-
-    respond_to do |format|
-        format.html { redirect_to :action => "index" }
-    end
-  end
-
-  # GET /my_tasks/drop/:id
-  def drop
-    @task = Task.find(params[:id])
-    @task.assignee = nil
-    
-    if @task.save then
-      flash[:notice] = 'Task dropped!'
-    else
-      flash[:warning] = 'Unable to drop task!'
-    end
-
-    respond_to do |format|
-        format.html { redirect_to :action => "index" }
-    end
-  end
     
   def find_tasks_by_assignee(assignee)
     Task.find(:all, :conditions => { :assignee_id => assignee })
