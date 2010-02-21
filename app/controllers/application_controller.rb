d09746c94c26e2cdc31138637aa2f1238a250d40
douglassquirrel <squirrel@blindpages.com>
2010-02-21 01:29:53 +0000

    Allow new req through without auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 
-  before_filter :authenticate, :except => [:phone, :create]
+  before_filter :authenticate, :except => [:phone, :create, :new]
 
   private
     def authenticate
