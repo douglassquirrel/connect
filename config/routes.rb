43353784c13ddcbfd749d17369baee8284f42650
douglassquirrel <squirrel@blindpages.com>
2010-05-03 12:31:44 +0100

    Home page

--- config/routes.rb
+++ config/routes.rb
@@ -51,4 +51,6 @@ ActionController::Routing::Routes.draw do |map|
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+
+  map.root :controller => 'tasks', :action => 'my'
 end
---
+++ start.sh
@@ -0,0 +1,1 @@
+script/server -d -p 9090 
